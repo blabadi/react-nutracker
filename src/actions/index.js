7ddb74795ed5e97bfd221f61e04361956c797388
Bashar <basharlabadi@gmail.com>
2018-09-08 19:11:36 -0400

    do async search food call add redux thunk

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export const SEARCH_FOOD = 'SEARCH_FOOD';
-
-export function searchFood(term) {
-    console.log(`in searchFood ${term}`);
-    return {
-        type: SEARCH_FOOD,
-        term
-    }
-}
